981d759671278c6d91bd9977495f6f9649de3b8d
Javier Anton <javier.yuste@strategybigdata.com>
2024-06-09 13:44:01 +0200

    Introduced some modifications for fixing it

--- example_write/test_pipeline.py
+++ example_write/test_pipeline.py
@@ -1,7 +1,6 @@
 import apache_beam as beam
 
 def run():
-    # Definir el pipeline
     with beam.Pipeline() as p:
         (p
          | 'Create' >> beam.Create(['Hello, World!'])
--- join_tables/execute_pipeline.sh
+++ join_tables/execute_pipeline.sh
@@ -5,6 +5,6 @@ python -m join_tables --project=$PROJECT_NAME \
     --region=$REGION \
     --staging_location=$BUCKET_NAME/staging \
     --temp_location=$BUCKET_NAME/temp \
-    --input-customers=$BUCKET_NAME/customers.csv \
-    --input-orders==$BUCKET_NAME/orders.csv \
+    --input_customers=$BUCKET_NAME/customers.csv \
+    --input_orders==$BUCKET_NAME/orders.csv \
     --output=$PROJECT_NAME:customers.information_advanced
